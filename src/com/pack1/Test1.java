08712fe14e9d33fcb80df8590a16cc51ebe69c1b
PritamX Kumar <pritamx.kumar@intel.com>
2019-09-22 13:36:49 +0530

    Create folder feature

--- src/com/pack1/Test1.java
+++ src/com/pack1/Test1.java
@@ -2,7 +2,14 @@ package com.pack1;
 
 public class Test1 
 {
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		System.out.println("Main of Test1");
+		craeteFolder();
+	}
+	
+	static void craeteFolder()
+	{
+		System.out.println("craeteFolder");
 	}
 }
